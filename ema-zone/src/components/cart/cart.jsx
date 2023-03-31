8c863c81071a8c3c6a53db76d8e9c56310dcd610
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-03-31 06:18:37 +0600

    retriped

--- ema-zone/src/components/cart/cart.jsx
+++ ema-zone/src/components/cart/cart.jsx
@@ -4,21 +4,23 @@ import "./cart.css"
 const Cart = (props) => {
     const {cart} = props;
     console.log(cart);
+
+    let quantity = 0;
     let totalPrice = 0;
     let totalShipping = 0;
     for (let product of cart){
         const price = product.price;
         totalPrice = price + totalPrice;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity+1;
     }
-
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + tax + totalShipping;
 
     return (
         <div className='cart'>
             <h2>Order summery</h2>
-            <p>Selected item: {cart.length}</p>
+            <p>Selected item: {quantity}</p>
             <p>Total Price: ${totalPrice.toFixed(2)}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- ema-zone/src/components/shop/Shop.jsx
+++ ema-zone/src/components/shop/Shop.jsx
@@ -6,33 +6,58 @@ import "./Shop.css"
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch("products.json")
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
 
-    useEffect(()=>{
+    // useEffect(()=>{
+    //     const storedCart = getShoppingCart();
+    //     console.log(storedCart);
+    //     // step 1: get id
+    //     for(const id in storedCart){
+    //         // step 2: get the product by using id
+    //         const addedProduct = products.find(product => product.id === id);
+    //         console.log(addedProduct , "add"); //****/ problem i don't get the object by using . notation
+    //         // step 3: get quantity of the product 
+    //         const quantity = storedCart[id];
+    //         addedProduct.quantity = quantity;
+    //     }
+    // },[products])
+
+    useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    },[])
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    }, [products])
 
     const [cart, setCart] = useState([]);
-    const handelAddToCart = (product) =>{
+    const handelAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id);
     }
-    
+
 
     return (
         <div className='shop-container'>
             <div className='porducts-container'>
                 {
-                    products.map(product => <Product 
+                    products.map(product => <Product
                         product={product}
                         key={product.id}
-                        handelAddToCart = {handelAddToCart}
+                        handelAddToCart={handelAddToCart}
                     ></Product>)
                 }
             </div>
